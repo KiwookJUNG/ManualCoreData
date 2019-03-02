abcb02e96019679cc88d6aba69ae56ecfcce87f0
정기욱 <rldnr56@me.com>
2019-03-02 15:32:03 +0900

    CoreData를 사용하게 위해 import로 coredata를 반입해주고
    엔티티가 정의된 데이터 모델 파일을 기반으로 영구저장소를 연결하고 이를 컨테이너 객체로 만들어 변화해준다.

--- ManualCoreData/AppDelegate.swift
+++ ManualCoreData/AppDelegate.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores{
+            if let error = $1 as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
